36cca5d5c281e88b1e78ee91ef4f6d9d2444b801
Bedivierre <bookvariant@list.ru>
2020-08-14 14:46:24 +0300

    Исправление получения статуса оплаты

--- src/Requests/Payment/PaymentStatusRequest.php
+++ src/Requests/Payment/PaymentStatusRequest.php
@@ -25,26 +25,17 @@ use Bedivierre\Craftsman\Masonry\BaseDataObject;
 /**
  * @package Bedivierre\Alfastrah\Requests\Payment
  * @property string _osago_uuid
- * @property string _order_id
+ * @property string order_id
  */
 class PaymentStatusRequest extends BaseRequest
 {
     public function __construct(string $uid, string $order_id, $testmode = null)
     {
-        $uri = str_replace('{osago_uuid}', $uid, AS_API::$payment) . "?order_id={$order_id}";
+        $uri = str_replace('{osago_uuid}', $uid, AS_API::$payment);
         $this->_osago_uuid = $uid;
-        $this->_order_id = $order_id;
+        $this->order_id = $order_id;
         parent::__construct($uri, 'get', $testmode);
 
-    }
-
-    /**
-     * @param string $order_id
-     * @return BaseResponseObject|BaseResponse|PaymentStatusResponse
-     */
-    public function status(){
-        $id = $this->_order_id;
-        return self::getStatus($this->_osago_uuid, $id);
     }
     /**
      * @param string $osago_uuid
@@ -52,9 +43,8 @@ class PaymentStatusRequest extends BaseRequest
      * @return BaseResponseObject|BaseResponse|PaymentStatusResponse
      */
     public static function getStatus(string $osago_uuid, string $order_id){
-        $method = str_replace("{osago_uuid}", $osago_uuid, AS_API::$payment);
-        $r = new BaseRequest($method . "?order_id={$order_id}", 'get');
-        return $r->get();
+        $r = new PaymentStatusRequest($osago_uuid, $order_id);
+        return $r->doRequest();
     }
 
     /**
